16bb659a6fbd1616b36ef609db4d2f1e292b3862
Gabriel Albuquerque <albuquerque.gabrielc@gmail.com>
2022-12-14 10:58:16 -0300

    feat: initial setup sanity

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -1,5 +1,8 @@
 import { defineCliConfig } from "sanity/cli";
 
+// const projectId = process.env.NEXT_PUBLIC_PROJECT_ID;
+// const dataset = process.env.NEXT_PUBLIC_DATASET;
+
 export default defineCliConfig({
   api: {
     projectId: "pjyt8nm1",
--- sanity.config.ts
+++ sanity.config.ts
@@ -3,13 +3,15 @@ import { deskTool } from "sanity/desk";
 import { visionTool } from "@sanity/vision";
 import { schemaTypes } from "./schemas";
 
-export default defineConfig({
-  name: "default",
-  title: "sanity-blog-next13",
+// const projectId = process.env.NEXT_PUBLIC_PROJECT_ID!;
+// const dataset = process.env.NEXT_PUBLIC_DATASET!;
 
+export default defineConfig({
+  basePath: "/studio",
+  name: "Blog",
+  title: "sanityBlogNext13",
   projectId: "pjyt8nm1",
   dataset: "production",
-
   plugins: [deskTool(), visionTool()],
 
   schema: {
