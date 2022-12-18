777b632d12a4a602b77b351b4f55de571ec5d777
Gabriel Albuquerque <albuquerque.gabrielc@gmail.com>
2022-12-18 09:04:36 -0300

    fix: 🔐 env file project ID security️

--- sanity.config.ts
+++ sanity.config.ts
@@ -7,16 +7,22 @@ import { StudioSanityNavbar } from "./app/components/StudioSanityNavbar";
 import { Logo } from "./app/components/Logo";
 import { getDefaultDocumentNode } from "./lib/sanity/structure";
 
-const projectId = process.env.NEXT_PUBLIC_PROJECT_ID;
+const projectId = process.env.NEXT_PUBLIC_PROJECT_ID!;
 const dataset = process.env.NEXT_PUBLIC_DATASET;
 const apiVersion = process.env.NEXT_PUBLIC_DATASET;
+
+if (projectId) {
+  console.log("Project ID: OK");
+} else {
+  throw new Error("Project ID is missing");
+}
 
 export default defineConfig<WorkspaceOptions>({
   basePath: "/studio",
   name: "Blog",
   title: "sanityBlogNext13",
   // Protect this! //Dot Env dont get recognized, i dont know why...
-  projectId: "pjyt8nm1",
+  projectId,
   dataset: "production",
   plugins: [
     deskTool({
